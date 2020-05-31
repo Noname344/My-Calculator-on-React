9ca6e66c7711319fd73274f428804de68a1c38fe
noname344 <myyzrik@gmail.com>
2020-05-31 21:37:26 +0300

    Updates

---
+++ precache-manifest.7bd8a4564e68491db3e44de624521030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc43ff38dea001c3f4b5689887c5165",
+    "url": "/Calculator-on-React/index.html"
+  },
+  {
+    "revision": "98dbd59c041f5c31483f",
+    "url": "/Calculator-on-React/static/css/main.2b38e0ef.chunk.css"
+  },
+  {
+    "revision": "5cb561074e2d78ed1046",
+    "url": "/Calculator-on-React/static/js/2.846e2a06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-on-React/static/js/2.846e2a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98dbd59c041f5c31483f",
+    "url": "/Calculator-on-React/static/js/main.4dda6d33.chunk.js"
+  },
+  {
+    "revision": "18e899eb88cb2abcb93d",
+    "url": "/Calculator-on-React/static/js/runtime-main.3ee5245e.js"
+  }
+]);
